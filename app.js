7dd362e9cdffc673686cdb22f1da538d7c458626
husnain3964 <husnain3964@gmail.com>
2024-08-25 03:50:39 +0500

    js basic task

---
+++ app.js
@@ -0,0 +1,26 @@
+// alert
+let a = alert('happy js user');
+
+// prompt and print at page
+let userValue =prompt("hello user : put your idea about js")
+document.write(userValue)
+
+// task1
+var street;
+var country;
+
+street = "street 4b SHS";
+country="canada";
+
+let fullMailingAddress= (street + " " + country);
+
+console.log(fullMailingAddress)
+document.write(fullMailingAddress)
+
+// one more method to print on console
+
+fullMailingAddress= (`${street}   ${country}`);
+console.log(fullMailingAddress);
+
+
+// taskone completed
