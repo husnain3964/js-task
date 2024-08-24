6147851e4f19c1ede6c0e3c200db182e6e95030d
husnain3964 <husnain3964@gmail.com>
2024-08-25 04:21:58 +0500

    add more js basic task

--- app.js
+++ app.js
@@ -23,4 +23,42 @@ fullMailingAddress= (`${street}   ${country}`);
 console.log(fullMailingAddress);
 
 
-// taskone completed
+// task one completed
+
+// task 2 started
+
+let score_1 = 12;
+let score_2 = 15;
+let score_3 = 78;
+
+console.log(`here is the score 1 value ${score_1}`);
+console.log(`here is the score 2 value ${score_2}`);
+console.log(`here is the score 3 value ${score_3}`);
+
+let sumOfTotal= score_1 + score_2 + score_3;
+
+console.log(`the total  all scores = ${sumOfTotal}`);
+
+let avg= (sumOfTotal/3)
+console.log(`the average of total score is ${avg}`);
+
+
+
+
+
+//task
+
+var plates = 20;
+var peoples= 7;
+
+console.log(`plates = ${plates}`);
+console.log(`peoples = ${peoples}`);
+
+let remaining = (  plates % peoples)
+console.log(`remaning plates after distribution ${remaining}`);
+
+remaining++
+console.log(`add one more in  remaining ${remaining}`);
+
+
+console.log(`  there are ${remaining} plates availbale `);
