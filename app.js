95219f299fc8634da8cf01006458f044ea8db780
husnain3964 <husnain3964@gmail.com>
2024-09-19 03:55:27 +0500

    function

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
 // alert
-let a = alert('happy js user');
+// let a = alert('happy js user');
 
 // prompt and print at page
-let userValue =prompt("hello user : put your idea about js")
-document.write(userValue)
+// let userValue =prompt("hello user : put your idea about js")
+// document.write(userValue)
 
 // task1
-var street;
-var country;
+// var street;
+// var country;
 
-street = "street 4b SHS";
-country="canada";
+// street = "street 4b SHS";
+// country="canada";
 
-let fullMailingAddress= (street + " " + country);
+// let fullMailingAddress= (street + " " + country);
 
-console.log(fullMailingAddress)
-document.write(fullMailingAddress)
+// console.log(fullMailingAddress)
+// document.write(fullMailingAddress)
 
 // one more method to print on console
 
-fullMailingAddress= (`${street}   ${country}`);
-console.log(fullMailingAddress);
+// fullMailingAddress= (`${street}   ${country}`);
+// console.log(fullMailingAddress);
 
 
 // task one completed
 
 // task 2 started
 
-let score_1 = 12;
-let score_2 = 15;
-let score_3 = 78;
+// let score_1 = 12;
+// let score_2 = 15;
+// let score_3 = 78;
 
-console.log(`here is the score 1 value ${score_1}`);
-console.log(`here is the score 2 value ${score_2}`);
-console.log(`here is the score 3 value ${score_3}`);
+// console.log(`here is the score 1 value ${score_1}`);
+// console.log(`here is the score 2 value ${score_2}`);
+// console.log(`here is the score 3 value ${score_3}`);
 
-let sumOfTotal= score_1 + score_2 + score_3;
+// let sumOfTotal= score_1 + score_2 + score_3;
 
-console.log(`the total  all scores = ${sumOfTotal}`);
+// console.log(`the total  all scores = ${sumOfTotal}`);
 
-let avg= (sumOfTotal/3)
-console.log(`the average of total score is ${avg}`);
+// let avg= (sumOfTotal/3)
+// console.log(`the average of total score is ${avg}`);
 
 
 
@@ -48,35 +48,132 @@ console.log(`the average of total score is ${avg}`);
 
 //task
 
-var plates = 20;
-var peoples= 7;
+// var plates = 20;
+// var peoples= 7;
 
-console.log(`plates = ${plates}`);
-console.log(`peoples = ${peoples}`);
+// console.log(`plates = ${plates}`);
+// console.log(`peoples = ${peoples}`);
 
-let remaining = (  plates % peoples)
-console.log(`remaning plates after distribution ${remaining}`);
+// let remaining = (  plates % peoples)
+// console.log(`remaning plates after distribution ${remaining}`);
 
-remaining++
-console.log(`add one more in  remaining ${remaining}`);
+// remaining++
+// console.log(`add one more in  remaining ${remaining}`);
 
 
-console.log(`  there are ${remaining} plates availbale `);
+// console.log(`  there are ${remaining} plates availbale `);
 
 // task-2 has completed
 
 //task 3 started
 //array
 
-let fruits;
-fruits=["apple" ,"mango" ," banana" , "graps" , "pineapple" ,"potato"]
-console.log("here is the fruits list" ); 
-console.log(fruits); 
+// let fruits;
+// fruits=["apple" ,"mango" ," banana" , "graps" , "pineapple" ,"potato"]
+// console.log("here is the fruits list" ); 
+// console.log(fruits); 
 
-console.log(`first fruits ${fruits[0]}`);
-console.log(`third fruits ${fruits[2]}`);
-console.log(`fifth fruits ${fruits[4]}`);
+// console.log(`first fruits ${fruits[0]}`);
+// console.log(`third fruits ${fruits[2]}`);
+// console.log(`fifth fruits ${fruits[4]}`);
 
-fruits[5] = "peach"
-console.log(`updated fruits list `); 
-console.log(fruits); 
+// fruits[5] = "peach"
+// console.log(`updated fruits list `); 
+// console.log(fruits); 
+
+// fruits.push(324234)
+// console.log(fruits);
+// fruits.pop()
+// console.log(fruits);
+// fruits.unshift(324234)
+// console.log(fruits);
+// fruits.shift()
+// console.log(fruits);
+
+// fruits.splice(1,0,'nofruits')
+// console.log(fruits);
+
+// let newFruits;
+// newFruits=fruits.slice(0,3);
+// console.log(newFruits);
+
+
+
+// function//////////////////////////////////////////
+
+// function printHello(){
+//     console.log('helloeeeeeeeeee');
+//     console.log('helloeeeeeeeeee');
+//     console.log('helloeeeeeeeeee');
+//     console.log('helloeeeeeeeeee');
+// }
+
+
+// printHello()
+
+
+
+// function greeting(name) {
+//     console.log(`hello ${name}`);
+// }
+
+// greeting('bob')
+
+
+
+
+// height = 20;
+// width=30;
+
+// function size(size01 ) {
+//     return (size01 *2.6)
+
+// }
+//   heightSize= size(height)
+//   widthSize= size(width)
+
+//   console.log([heightSize,widthSize]);
+
+
+
+// function twoVal(a,b){
+// return [a*2 , b+5]
+// }
+
+// num1=3
+// num2=5
+//  val =twoVal(num1,num2)
+// console.log(val);
+
+
+
+
+// function total(subtotal , tax){
+//     return (subtotal +tax)
+
+// }
+
+// num1=20
+// num2=10
+
+// calulate = total(num1 ,num2)
+// console.log(calulate);
+
+
+// var order1;
+// var order2;
+// var order3;
+
+// function orders(){
+//     console.log(order1 + order2 + order3);
+// }
+
+
+// order1=20
+// order2=5
+// order3=calulate
+
+
+
+
+// orders()
